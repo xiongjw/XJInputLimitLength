d11830a953001064ced568e157a35a2b584d6c62
xiongjiwei <877703715@qq.com>
2019-05-09 15:07:32 +0800

    first commit

---
+++ XJInputLimitLength/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJInputLimitLength
+//
+//  Created by mac on 2019/5/9.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XJInputLimitLength/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XJInputLimitLength
+//
+//  Created by mac on 2019/5/9.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
